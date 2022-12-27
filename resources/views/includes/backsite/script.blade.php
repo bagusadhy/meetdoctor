2bc7e6e0131dcea211eb23cca1f8ad40a41afd21
Bagusadhy_ <bagusadhy182@gmail.com>
2022-12-27 12:52:53 +0700

    add includes for backsite

---
+++ resources/views/includes/backsite/script.blade.php
@@ -0,0 +1,25 @@
+<!-- Bootstrap JS -->
+<script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous">
+</script>
+
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous">
+</script>
+
+{{-- Vendor --}}
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+        $(".sidebarCollapseDefault").on("click", function () {
+            $(".sidebar").toggleClass("active");
+            $(".content").toggleClass("active");
+        });
+    });
+</script>
---
+++ resources/views/includes/backsite/style.blade.php
@@ -0,0 +1,16 @@
+
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css"/>
+
+{{-- Vendor --}}
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
+
+<!-- External CSS -->
+<link rel="stylesheet" href="{{ asset('assets/backsite/app-assets/css/styles.css') }}" type="text/css" media="screen"/>
+
+<!-- CDN Fontawesome -->
+<script src="https://kit.fontawesome.com/32f82e1dca.js"crossorigin="anonymous"></script>
