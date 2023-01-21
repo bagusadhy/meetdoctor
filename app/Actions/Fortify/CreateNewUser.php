f333c011603a27b5cf56484b34865a3bff96a7e4
Bagusadhy_ <bagusadhy182@gmail.com>
2023-01-22 01:07:19 +0700

    create detail user and user role when register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,6 +3,9 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use App\Models\ManagementAccess\DetailUser;
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -27,10 +30,32 @@ class CreateNewUser implements CreatesNewUsers
             // 'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+        try {
+            //code...
+            return DB::transaction(function () use ($input) {
+                return tap(User::create([
+                    'name' => $input['name'],
+                    'email' => $input['email'],
+                    'password' => Hash::make($input['password']),
+                ]), function (User $user) {
+
+                    // add to role user data
+                    $user->role()->sync(5);
+
+                    // add to detail users
+                    $detail_user = new DetailUser;
+                    $detail_user->user_id = $user->id;
+                    $detail_user->type_user_id = 3;
+                    $detail_user->contact = NULL;
+                    $detail_user->address = NULL;
+                    $detail_user->photo = NULL;
+                    $detail_user->gender = NULL;
+                    $detail_user->save();
+                });
+            });
+        } catch (\Throwable $th) {
+            //throw $th;
+            return redirect(route('register'));
+        }
     }
 }
