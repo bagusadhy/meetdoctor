f6472261a2aa1ab507be981b3296a706236ebce9
Bagusadhy_ <bagusadhy182@gmail.com>
2023-01-30 08:09:22 +0700

    fix user password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,25 +27,25 @@ class UserSeeder extends Seeder
             [
                 'name' => 'Dr. Bagus',
                 'email' => 'bagus@gmail.com',
-                'password' => Hash::make('bagus.@gmail.com'),
+                'password' => Hash::make('bagus@gmail.com'),
                 'remember_token' => null,
             ],
             [
                 'name' => 'Dr. Herman',
                 'email' => 'herman@gmail.com',
-                'password' => Hash::make('herman.@gmail.com'),
+                'password' => Hash::make('herman@gmail.com'),
                 'remember_token' => null,
             ],
             [
                 'name' => 'Dr. Ari',
                 'email' => 'ari@gmail.com',
-                'password' => Hash::make('ari.@gmail.com'),
+                'password' => Hash::make('ari@gmail.com'),
                 'remember_token' => null,
             ],
             [
                 'name' => 'Dr. Andre',
                 'email' => 'andre@gmail.com',
-                'password' => Hash::make('andre.@gmail.com'),
+                'password' => Hash::make('andre@gmail.com'),
                 'remember_token' => null,
             ],
             [
