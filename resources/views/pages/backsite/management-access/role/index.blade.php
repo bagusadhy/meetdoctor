d4ac5454c178697d18f398cf097bad9097fcb786
Bagusadhy_ <bagusadhy182@gmail.com>
2023-02-05 13:55:38 +0700

    change style

--- resources/views/pages/backsite/management-access/role/index.blade.php
+++ resources/views/pages/backsite/management-access/role/index.blade.php
@@ -9,30 +9,53 @@
 
         <section class="p-3">
 
-            {{-- errors --}}
-            @if($errors->any())
+            <header>
+                <h3>Role</h3>
+                <p>Manage data for Role</p>
+            </header>
+
+             {{-- error --}}
+            @if ($errors->any())
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 
                     <ul>
-                        @foreach ($errors->all() as $error )
+                        @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
                         @endforeach
                     </ul>
                 </div>
             @endif
 
-            <header>
-                <h3>Role</h3>
-                <p>Manage data for Role</p>
-            </header>
-
-            @can('role_create')
-                <div class="">
-                    <button onclick="event.preventDefault(); $('#form-role').attr('action', '{{ route('role.store') }}');
-                    $('#modal-role').modal('show');" class="btn btn-primary mb-4">Add New Role</button>
+             @can('role_create')
+                <div class="accordion mb-3" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Add Role
+                        </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body col-8 mx-auto">
+                                <form action="{{ route('role.store') }}" id="form-role" method="POST">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="title" class="form-label">Role Title<code style="color: red">*</code></label>
+                                        <input type="text" class="form-control" id="title" name="title" required>
+                                        @if($errors->has('title'))
+                                            <p style="font-style: bold; color: red;">{{ $errors->first('title') }}</p>
+                                        @endif
+                                    </div>
+                                    <div class="d-flex justify-content-end">
+                                        <input type="submit" class="btn btn-primary btn-sm" style="width: 100px" value="add"></input>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             @endcan
+
 
             @can('role_table')
                 <div class="table-responsive shadow p-3 mb-5 bg-body rounded">
@@ -53,29 +76,43 @@
                                     <td>{{ count($r->permission) }} Permission</td>
                                     <td>
                                         <div class="text-center">
-                                            @can('role_show')
-                                                <a href="{{ route('role.show', $r->id) }}"
-                                                    data-remote=""
-                                                    id="role-show"
-                                                    class="btn btn-sm btn-success">
-                                                    Detail
-                                                </a>
-                                            @endcan
+                                            <div class="btn-group">
+                                                <button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    Action
+                                                </button>
+                                                <ul class="dropdown-menu">
+                                                  
+                                                    {{-- show --}}
+                                                    <li>
+                                                        @can('role_show')
+                                                        <a href="{{ route('role.show', $r->id) }}" class="dropdown-item">Detail</a>
+                                                        @endcan
+                                                        <hr class="dropdown-divider">
+                                                    </li>
 
-                                            @can('role_edit')
-                                                <a href="{{ route('role.edit', $r->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                                            @endcan
+                                                    {{-- edit --}}
+                                                    <li>
+                                                        @can('role_edit')
+                                                        <a href="{{ route('role.edit', $r->id) }}" class="dropdown-item">Edit</a>
+                                                        @endcan
+                                                    </li>
 
-                                            @if ($r->id > 2)
-                                                @can('role_delete')
-                                                    <button onclick="event.preventDefault(); $('#form-delete').attr('action', '{{ route('role.destroy', $r->id) }}'); document.getElementById('form-delete').submit()" class="btn btn-sm btn-danger">Delete
-                                                        <form action="" id="form-delete" method="post" style="display: none">
-                                                            @csrf
-                                                            @method('delete')
-                                                        </form>
-                                                    </button>
-                                                @endcan
-                                            @endif
+                                                    {{-- delete --}}
+                                                    <li>
+                                                        @if ($r->id > 2)
+                                                        <li><hr class="dropdown-divider"></li>
+                                                        @can('role_delete')
+                                                            <button onclick="event.preventDefault(); $('#form-delete').attr('action', '{{ route('role.destroy', $r->id) }}'); document.getElementById('form-delete').submit()" class="dropdown-item">Delete
+                                                                <form action="" id="form-delete" method="post" style="display: none">
+                                                                    @csrf
+                                                                    @method('delete')
+                                                                </form>
+                                                            </button>
+                                                        @endcan
+                                                        @endif
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
@@ -94,28 +131,6 @@
         </section>
     </main>
 
-    <div class="modal" tabindex="-1" id="modal-role">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Add New Role</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="" id="form-role" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Role Tittle</label>
-                        <input type="text" class="form-control" id="title" name="title">
-                        </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">cancel</button>
-                    <input type="submit" class="btn btn-primary" value="add"></input>
-                </div>
-            </form>
-        </div>
-    </div>
 
 @endsection
 
